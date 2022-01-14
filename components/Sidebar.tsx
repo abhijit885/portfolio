b6113709f6b0435263692fc260bccecfa479cc7e
abhijit <abhijitsaha885@gmail.com>
2022-01-14 13:17:25 +0530

    @abhijit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         quality="100"
       />
       <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
-        <span className="text-green ">Abhijit</span> Saha
+        <span className="text-green ">Abhijit</span>Abhijit
       </h3>
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
         Web Developer
